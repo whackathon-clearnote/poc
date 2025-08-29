90c5936e20132197eecf7b88153e557318e93215
J-hta-n <e0969121@u.nus.edu>
2025-08-29 23:57:29 +0800

    feat: modify layout ui for mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 import { AppBar, Toolbar, Typography } from "@mui/material";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
+  const pathname = usePathname();
+
   return (
     <AppBar position="absolute" sx={{ bgcolor: "darkred" }}>
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          Epic Systems Simulation (Pharmacist&apos;s POV)
+          {pathname === "/mobile"
+            ? "Mobile App Mockup (Patient & Caregiver's POV)"
+            : "Epic Systems Simulation (Pharmacist's POV)"}
         </Typography>
       </Toolbar>
     </AppBar>
